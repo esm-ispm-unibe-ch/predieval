67454b1ccc61b6d27b5d4195f2f7ab6dd75ab3f8
oremiou <oremiou@gmail.com>
2022-11-01 15:27:39 +0100

    changed data

---
+++ R/databinary.R
@@ -0,0 +1,11 @@
+#' Simulated dataset, binary outcome
+#'
+#' @docType data
+#'
+#' @usage data(databinary)
+#'
+#' @examples
+#' data(databinary)
+#' head(databinary)
+"databinary"
+
---
+++ R/datacont.R
@@ -0,0 +1,10 @@
+#' Simulated dataset, continuous outcome
+#'
+#' @docType data
+#'
+#' @usage data(datcont)
+#'
+#' @examples
+#' data(datacont)
+#' head(datacont)
+"datacont"
